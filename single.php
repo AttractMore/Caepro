69e9a524d7e796f9ac1e3cff5014098cba8b499e
Roger Knight <roger@rogerknight.info>
2024-03-26 14:58:24 +0100

    Fix error realted to gravatar sizing

--- single.php
+++ single.php
@@ -4,7 +4,9 @@
 	<!-- section -->
 	<section>
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+	<?php if (have_posts()):
+   while (have_posts()):
+     the_post(); ?>
 
 	<section class="postMeta">
 		<div class="l-container">
@@ -13,40 +15,52 @@
 				<div class="postMetaCategoryName">
 				<ul>
 				<?php
-       $categories = get_the_category();
-	   $separator = ' ';
-	   $output = '';
-	   if ( ! empty( $categories ) ) {
-		   foreach( $categories as $category ) {
-			   $output .= '<li><a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a></li>' . $separator;
-		   }
-		   echo trim( $output, $separator );
-	   }?>
+    $categories = get_the_category();
+    $separator = " ";
+    $output = "";
+    if (!empty($categories)) {
+      foreach ($categories as $category) {
+        $output .=
+          '<li><a href="' .
+          esc_url(get_category_link($category->term_id)) .
+          '" alt="' .
+          esc_attr(sprintf(__("View all posts in %s", "textdomain"), $category->name)) .
+          '">' .
+          esc_html($category->name) .
+          "</a></li>" .
+          $separator;
+      }
+      echo trim($output, $separator);
+    }
+    ?>
    </ul>
 				</div>
 			</div>
 			<div class="postMetaDate">
 				<div class="postMetaCategoryTitle">Date</div>
-				<div class="postMetaCategoryName"><?php the_date('dS M Y');?></div>
+				<div class="postMetaCategoryName"><?php the_date("dS M Y"); ?></div>
 			</div>
 		</div>
 	</section>
 
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<?php the_content();?>
+		<?php the_content(); ?>
 
 
 	
 		<div class="authorBox">
-			<div class="author-image"><?php echo get_avatar( get_the_author_meta( 'ID' ), auto ); ?></div>
+			<div class="author-image"><?php echo get_avatar(get_the_author_meta("ID")); ?></div>
 			<div class="author-desc">
 				<span class="tagline">Author</span>
-				<h2><?php echo the_author_meta( 'display_name' ); ?></h2>
-				<p><?php $authorDesc = the_author_meta('description'); echo $authorDesc; ?></p>
+				<h2><?php echo the_author_meta("display_name"); ?></h2>
+				<p><?php
+    $authorDesc = the_author_meta("description");
+    echo $authorDesc;
+    ?></p>
 			</div>
 			<div class="author-linkedin">
-				<a href="<?php the_author_meta('linkedin'); ?>">
+				<a href="<?php the_author_meta("linkedin"); ?>">
 					<img src="/wp-content/uploads/2022/12/174857.png" />
 				</a>
 			</div>
@@ -62,18 +76,21 @@
 				<div class="l-container relatedPostsContainer">
 				
 				<?php
-
-					$related = get_posts( array( 'category__in' => wp_get_post_categories($post->ID), 'numberposts' => 5, 'post__not_in' => array($post->ID) ) );
-					if( $related ) foreach( $related as $post ) {
-					setup_postdata($post); ?>
-					<div class="news-item" style="background-image:url('<?php the_post_thumbnail_url('full');?>');background-size: cover; ">
+    $related = get_posts(["category__in" => wp_get_post_categories($post->ID), "numberposts" => 5, "post__not_in" => [$post->ID]]);
+    if ($related) {
+      foreach ($related as $post) {
+        setup_postdata($post); ?>
+					<div class="news-item" style="background-image:url('<?php the_post_thumbnail_url("full"); ?>');background-size: cover; ">
 							<div class="news-item-content">
-								<h3><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
+								<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 								<a href="<?php the_permalink(); ?>" class="newsLink">Read more </a>
 							</div>
 						</div>
-				<?php }
-				wp_reset_postdata(); ?>
+				<?php
+      }
+    }
+    wp_reset_postdata();
+    ?>
 
 
 				</div>	
@@ -81,19 +98,23 @@
 		</section>
 		<!-- /article -->
 
-	<?php endwhile; ?>
+	<?php
+   endwhile; ?>
 
-	<?php else: ?>
+	<?php
+ else:
+    ?>
 
 		<!-- article -->
 		<article>
 
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
+			<h1><?php _e("Sorry, nothing to display.", "html5blank"); ?></h1>
 
 		</article>
 		<!-- /article -->
 
-	<?php endif; ?>
+	<?php
+ endif; ?>
 
 	</section>
 	<!-- /section -->
